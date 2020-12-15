ca5f7686d2042faa7a7ed3e42286d9b029994ec9
975877475@qq.com <975877475@qq.com>
2020-12-15 17:43:23 +0800

    初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+import { StarsComponent } from './stars/stars.component';
+import { ProductComponent } from './product/product.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    StarsComponent,
+    ProductComponent,
+    NavbarComponent,
+    CarouselComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
