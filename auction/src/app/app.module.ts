1bfac71b5344a2715a8b5ef588120faaffa86c96
975877475@qq.com <975877475@qq.com>
2020-12-17 16:01:16 +0800

    3-9 路由实战思路 1.创建商品详情组件,显示商品的图片和标题 2.重构代码,把轮播图组件和商品列表组件封装进新的Home组件。 3.配置路由,在导航到商品详情组件时传递商品的标题参数。 4.修改App组件,根据路由显示Home组件或商品详情组件。 5修改商品列表组件,给商品标题添加带 route Link指令的链接,导航到商品详情路由。

--- auction/src/app/app.module.ts
+++ auction/src/app/app.module.ts
@@ -1,30 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {StarsComponent} from './stars/stars.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {SearchComponent} from './search/search.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { FooterComponent } from './footer/footer.component';
-import { StarsComponent } from './stars/stars.component';
-import { ProductComponent } from './product/product.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { SearchComponent } from './search/search.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ProductComponent} from './product/product.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+    {path = '', component: HomeComponent},
+    {path = 'product/:productTitle', component: ProductDetailComponent},
+
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    StarsComponent,
-    ProductComponent,
-    NavbarComponent,
-    CarouselComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule,
-      FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        StarsComponent,
+        ProductComponent,
+        NavbarComponent,
+        CarouselComponent,
+        SearchComponent,
+        ProductDetailComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(routes),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+// @ts-ignore
+export class AppModule {
+}
