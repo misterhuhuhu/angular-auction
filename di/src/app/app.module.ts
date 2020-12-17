5012956ab151f898c9a19f517ad739f299c4cd2e
胡昊宁 <975877475@qq.com>
2020-12-17 20:24:03 +0800

    4-5 使用工厂和值声明提供器

--- di/src/app/app.module.ts
+++ di/src/app/app.module.ts
@@ -6,18 +6,33 @@ import {ProductInfoComponent} from './product-info/product-info.component';
 import {ProductService} from './shared/product.service';
 import {ProductGridComponent} from './product-grid/product-grid.component';
 import {LoggerService} from './shared/logger.service';
+import {AnotherProductServiceService} from './shared/another-product-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductInfoComponent,
-    ProductGridComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [{provide: ProductService, useClass: ProductService}, LoggerService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductInfoComponent,
+        ProductGridComponent,
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [{
+        provide: ProductService,
+        useFactory: (logger: LoggerService, isDev) => {
+            if (isDev.isDev) {
+                return new ProductService(logger);
+            } else {
+                return new AnotherProductServiceService(logger);
+            }
+        },
+        deps: [LoggerService, 'IS_DEV_ENV']
+    },
+        LoggerService,
+        {provide: 'IS_DEV_ENV', useValue: {isDev: false}}
+        // {provide: 'IS_DEV_ENV', useValue: false}
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- di/src/app/product-grid/product-grid.component.ts
+++ di/src/app/product-grid/product-grid.component.ts
@@ -1,25 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {Product, ProductService} from '../shared/product.service';
-import {AnotherProductServiceService} from '../shared/another-product-service.service';
 
 @Component({
-  selector: 'app-product-grid',
-  templateUrl: './product-grid.component.html',
-  styleUrls: ['./product-grid.component.css'],
-  providers: [{
-    provide: ProductService, useClass: AnotherProductServiceService
-  }]
+    selector: 'app-product-grid',
+    templateUrl: './product-grid.component.html',
+    styleUrls: ['./product-grid.component.css'],
+    providers: [{provide: ProductService}]
 })
 export class ProductGridComponent implements OnInit {
 
-  product: Product;
+    product: Product;
 
-  constructor(private productService: ProductService) {
-    this.product = productService.getProduct();
-  }
+    constructor(private productService: ProductService) {
+        this.product = productService.getProduct();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 
 }
