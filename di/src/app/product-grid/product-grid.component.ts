560b8c03c9cc001abf78f0843a8caaccd8d3c14c
胡昊宁 <975877475@qq.com>
2020-12-17 20:33:42 +0800

    dev-4-5-注入器及层级关系

--- di/src/app/product-grid/product-grid.component.ts
+++ di/src/app/product-grid/product-grid.component.ts
@@ -12,10 +12,10 @@ export class ProductGridComponent implements OnInit {
     product: Product;
 
     constructor(private productService: ProductService) {
-        this.product = productService.getProduct();
     }
 
     ngOnInit(): void {
+        this.product = this.productService.getProduct();
     }
 
 
--- di/src/app/product-info/product-info.component.ts
+++ di/src/app/product-info/product-info.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
 import {Product, ProductService} from '../shared/product.service';
 
 @Component({
-  selector: 'app-product-info',
-  templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.css']
+    selector: 'app-product-info',
+    templateUrl: './product-info.component.html',
+    styleUrls: ['./product-info.component.css']
 })
 export class ProductInfoComponent implements OnInit {
 
-  product: Product;
+    product: Product;
+    private productService: ProductService;
 
-  constructor(private productService: ProductService) {
-    this.product = productService.getProduct();
-  }
+    constructor(private injector: Injector) {
+        this.productService = this.injector.get(ProductService);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.product = this.productService.getProduct();
+    }
 
 }
