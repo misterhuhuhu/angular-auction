62253ff816b0a19ee27c2afa0fec7a27835cffad
975877475@qq.com <975877475@qq.com>
2020-12-17 10:51:56 +0800

    第三章 3-2 路由

--- router/src/app/app-routing.module.ts
+++ router/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductComponent} from './product/product.component';
+import {Code404Component} from './code404/code404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // path=路径 component=组件
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'product', component: ProductComponent
+    },
+    {
+        // 访问的路由不存在时,将展示 Code404Component
+        // 这个通配符**必须是最后匹配的
+        path: '**', component: Code404Component
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- router/src/app/app.component.ts
+++ router/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'router';
+    title = 'router';
+
+    constructor(private router: Router) {
+    }
+    toProduct(): void {
+        // 与 <a [routerLink]="['/product']">商品详情</a>一样
+        this.router.navigate(['/product']);
+    }
 }
